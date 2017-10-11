673101739e10863af8c4f5adf1f089bfab766d9f
coreyfedde <coreysfedde@gmail.com>
2017-10-11 14:59:54 -0400

    Adds form to change city
    - Adds form that changes the city/location

--- src/WeatherBoard.js
+++ src/WeatherBoard.js
@@ -5,17 +5,32 @@ class WeatherBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weather: ""
+      weather: "",
+      city: 'Boston',
+      newCity: ''
     }
   }
+  cityChange (e) {
+      this.setState({
+        newCity: e.target.value
+      })
+      console.log(this.state.newCity)
+      console.log(this.state.city)
+    }
+    addCity (e) {
+      e.preventDefault()
+      this.setState({
+        city: this.state.newCity,
+      })
+    }
+    // let cityLocation = ''
+    // let date = ''
   componentDidMount() {
   // save a reference to `this` because the value of `this` will change
   // inside the different callback functions.
   var base = this
 
   // fetch weather
-  let cityLocation = ''
-  let date = ''
   let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=boston&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98';
   fetch(weatherApi)
     .then((response) => {
@@ -29,7 +44,19 @@ class WeatherBoard extends Component {
   render() {
     return (
       <div>
-        <div className="location">Location</div>
+        <form>
+          <label className="location">
+            Location:
+            <input
+            type="text"
+            placeholder="Type an item here"
+            onChange={(e) => this.cityChange(e)}
+            value={this.state.newCity}
+            />
+          </label>
+          <input onClick={(e) => this.addCity(e)} type="submit" value="Submit" />
+        </form>
+        <div >Location</div>
         <div className="date">Date</div>
         <div className="time">Time</div>
         <h1>Weather in Boston: {this.state.weather} Degrees</h1>
