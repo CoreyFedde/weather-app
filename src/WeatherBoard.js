0bc8a3f424f505cffea3888609f7379d2a7c714b
coreyfedde <coreysfedde@gmail.com>
2017-10-18 19:27:43 -0400

    Removes date selection input
    - Decided to only do day of data
    - This will help the user just pull up the app and have it immediately
    say what kind of weather is expected today

--- src/WeatherBoard.js
+++ src/WeatherBoard.js
@@ -25,31 +25,41 @@ class WeatherBoard extends Component {
         city: this.state.newCity,
       })
     }
-    dateChange (e) {
-      this.setState({
-        newDate: e.target.value
-      })
-      console.log(this.state.newDate)
-      console.log(this.state.date)
-    }
-    addDate (e) {
-      e.preventDefault()
-      this.setState({
-        date: this.state.newDate,
-      })
-    }
+    // dateChange (e) {
+    //   this.setState({
+    //     newDate: e.target.value
+    //   })
+    //   console.log(this.state.newDate)
+    //   console.log(this.state.date)
+    // }
+    // addDate (e) {
+    //   e.preventDefault()
+    //   this.setState({
+    //     date: this.state.newDate,
+    //   })
+    // }
     // let cityLocation = ''
     // let date = ''
   componentDidMount() {
+    let today = new Date()
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+
+    this.setState({
+      date: dateTime,
+    })
   // save a reference to `this` because the value of `this` will change
   // inside the different callback functions.
   var base = this
 
   // fetch weather
-  // let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=boston&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98';
-  let weatherApi = 'http://api.openweathermap.org/data/2.5/forecast?q=London,us&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98'
+  let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=boston&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98';
+  // let weatherApi = 'http://api.openweathermap.org/data/2.5/forecast?q=London,us&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98'
+  // let weatherApi = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
   fetch(weatherApi)
     .then((response) => {
+      console.log('date', this.state.date)
       return response.json()
     }).then((response) => console.log(response))
     // }).then((json) => {
@@ -60,6 +70,18 @@ class WeatherBoard extends Component {
 }
   render() {
     return (
+      // Below is the code for a date selection box that is no longer needed
+      // <form>
+      //   <label className="date">
+      //     Date:
+      //     <input
+      //     type="datetime-local"
+      //     onChange={(e) => this.dateChange(e)}
+      //     value={this.state.newDate}
+      //     />
+      //   </label>
+      //   <input onClick={(e) => this.addDate(e)} type="submit" value="Submit" />
+      // </form>
       <div>
         <form>
           <label className="location">
@@ -73,18 +95,6 @@ class WeatherBoard extends Component {
           </label>
           <input onClick={(e) => this.addCity(e)} type="submit" value="Submit" />
         </form>
-        <form>
-          <label className="date">
-            Date:
-            <input
-            type="datetime-local"
-            onChange={(e) => this.dateChange(e)}
-            value={this.state.newDate}
-            />
-          </label>
-          <input onClick={(e) => this.addDate(e)} type="submit" value="Submit" />
-        </form>
-        <div className="time">Time</div>
         <h1>Weather in Boston: {this.state.weather} Degrees</h1>
         <div className="clothes">Clothes</div>
       </div>
