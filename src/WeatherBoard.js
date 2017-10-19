42d05c30ac924974f55a0c60763c6ae78513f103
coreyfedde <coreysfedde@gmail.com>
2017-10-18 21:41:20 -0400

    Adds functionality for getting location and weather
    - Added fetch to Google's reverse Geocode API to get city name
    - City name changes the city of the state
    - Once that is finished in .then, getWeather function is called
    - GetWeather completes URL by using the city name from this.state.city
    - Then changes the this.state.weather to reflect results of weather API

--- src/WeatherBoard.js
+++ src/WeatherBoard.js
@@ -5,8 +5,8 @@ class WeatherBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weather: "",
-      city: 'Boston',
+      weather: 'Gathering weather...',
+      city: 'Gathing location...',
       newCity: '',
       date: '',
       newDate: ''
@@ -47,70 +47,70 @@ class WeatherBoard extends Component {
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     let dateTime = date+' '+time;
 
+    // save a reference to `this` because the value of `this` will change
+    // inside the different callback functions.
+    var base = this
+
     // Changes the state to reflect current data and time
-    this.setState({
+    base.setState({
       date: dateTime,
     })
 
-    // Grabs user's location on page load
-    // Function to check if the HTML geolocation is accessible
+    const showCity = function(position){
+      var x = position.coords.latitude;
+      var y = position.coords.longitude;
+      var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+x+','+y+'&sensor=true';
+      fetch(url)
+        .then((response) => {
+          return response.json()
+        }).then((data) => {
+          base.setState({ city: data.results[0].address_components[3].short_name })
+          console.log(base.state.city)
+        }).then(() => getWeather())
+        .catch((ex) => {
+          console.log('Error!', ex)
+        })
+    };
+
+    // function to handle error
+    const errorFunction = function(error){
+      var errorMessage = 'Unknown error';
+      switch(error.code) {
+        case 1:
+          errorMessage = 'Permission denied';
+          break;
+        case 2:
+          errorMessage = 'Position unavailable';
+          break;
+        case 3:
+          errorMessage = 'Timeout';
+          break;
+      }
+      console.log(errorMessage);
+    };
+
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 100000,
+      maximumAge: 0
+    };
+
     function getLocation() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition, errorFunction);
+            navigator.geolocation.getCurrentPosition(showCity, errorFunction, options);
         } else {
             console.log("Geolocation is not supported by this browser.")
         }
     }
-    // If accessible, calls this function to get the coordinates
-    function showPosition(position) {
-      let lat = position.coords.latitude
-      let lon = position.coords.longitude
-      console.log(lat)
-      console.log(lon)
-    }
-    // Function to handle error
-    function errorFunction (error) {
-      console.log('error', error)
-    }
-    // Calls the function when componentDidMount
-    getLocation()
-// .then((data) => console.log(data))
-
-//     function geoFindMe() {
-//   if (!navigator.geolocation){
-//     console.log('Geolocation is not supported by your browser')
-//   }
-//
-//   function success(position) {
-//     var latitude  = position.coords.latitude;
-//     var longitude = position.coords.longitude;
-//
-//     console.log('Latitude is ' + latitude + 'Longitude is ' + longitude)
-//
-//     // var img = new Image();
-//     // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-//     //
-//     // output.appendChild(img);
-//   }
-//
-//   function error() {
-//     console.log("Unable to retrieve your location")
-//   }
-//
-//   navigator.geolocation.getCurrentPosition(success, error);
-// }
-
-  // save a reference to `this` because the value of `this` will change
-  // inside the different callback functions.
-  var base = this
-
+    // Function to get weather
+ const getWeather = function() {
   // fetch weather
-  let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=boston&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98';
+  let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=' + base.state.city + '&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98';
   // let weatherApi = 'http://api.openweathermap.org/data/2.5/forecast?q=London,us&units=imperial&appid=052f26926ae9784c2d677ca7bc5dec98'
   // let weatherApi = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
   fetch(weatherApi)
     .then((response) => {
-      console.log('date', this.state.date)
+      console.log('date', base.state.date)
       return response.json()
     // }).then((response) => console.log(response))
     }).then((json) => {
@@ -118,6 +118,8 @@ class WeatherBoard extends Component {
     }).catch((ex) => {
       console.log('An error occured while parsing!', ex)
     })
+  }
+  getLocation()
 }
   render() {
     return (
@@ -147,7 +149,7 @@ class WeatherBoard extends Component {
       //   <input onClick={(e) => this.addCity(e)} type="submit" value="Submit" />
       // </form>
       <div>
-        <h1>Weather in Boston: {this.state.weather} Degrees</h1>
+        <h1>Weather in {this.state.city}: {this.state.weather} Degrees</h1>
         <div className="clothes">Clothes</div>
       </div>
     );
