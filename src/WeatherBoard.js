691455d704e4c9463f302e2f851140ef54970b70
coreyfedde <coreysfedde@gmail.com>
2017-10-18 20:17:09 -0400

    Removes location input and uses Geolocation
    - Removed location input
    - Added HTML geolocation to prompt user for access and then return their
    longitude and latitude

--- src/WeatherBoard.js
+++ src/WeatherBoard.js
@@ -41,14 +41,65 @@ class WeatherBoard extends Component {
     // let cityLocation = ''
     // let date = ''
   componentDidMount() {
+    // Grabs current date and time
     let today = new Date()
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
 
+    // Changes the state to reflect current data and time
     this.setState({
       date: dateTime,
     })
+
+    // Grabs user's location on page load
+    // Function to check if the HTML geolocation is accessible
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, errorFunction);
+        } else {
+            console.log("Geolocation is not supported by this browser.")
+        }
+    }
+    // If accessible, calls this function to get the coordinates
+    function showPosition(position) {
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+      console.log(lat)
+      console.log(lon)
+    }
+    // Function to handle error
+    function errorFunction (error) {
+      console.log('error', error)
+    }
+    // Calls the function when componentDidMount
+    getLocation()
+// .then((data) => console.log(data))
+
+//     function geoFindMe() {
+//   if (!navigator.geolocation){
+//     console.log('Geolocation is not supported by your browser')
+//   }
+//
+//   function success(position) {
+//     var latitude  = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//
+//     console.log('Latitude is ' + latitude + 'Longitude is ' + longitude)
+//
+//     // var img = new Image();
+//     // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+//     //
+//     // output.appendChild(img);
+//   }
+//
+//   function error() {
+//     console.log("Unable to retrieve your location")
+//   }
+//
+//   navigator.geolocation.getCurrentPosition(success, error);
+// }
+
   // save a reference to `this` because the value of `this` will change
   // inside the different callback functions.
   var base = this
@@ -61,12 +112,12 @@ class WeatherBoard extends Component {
     .then((response) => {
       console.log('date', this.state.date)
       return response.json()
-    }).then((response) => console.log(response))
-    // }).then((json) => {
-    //     base.setState({ weather: json.main.temp });
-    // }).catch((ex) => {
-    //   console.log('An error occured while parsing!', ex)
-    // })
+    // }).then((response) => console.log(response))
+    }).then((json) => {
+        base.setState({ weather: json.main.temp });
+    }).catch((ex) => {
+      console.log('An error occured while parsing!', ex)
+    })
 }
   render() {
     return (
@@ -82,19 +133,20 @@ class WeatherBoard extends Component {
       //   </label>
       //   <input onClick={(e) => this.addDate(e)} type="submit" value="Submit" />
       // </form>
+      // Below this is the code for the location input
+      // <form>
+      //   <label className="location">
+      //     Location:
+      //     <input
+      //     type="text"
+      //     placeholder="Type an item here"
+      //     onChange={(e) => this.cityChange(e)}
+      //     value={this.state.newCity}
+      //     />
+      //   </label>
+      //   <input onClick={(e) => this.addCity(e)} type="submit" value="Submit" />
+      // </form>
       <div>
-        <form>
-          <label className="location">
-            Location:
-            <input
-            type="text"
-            placeholder="Type an item here"
-            onChange={(e) => this.cityChange(e)}
-            value={this.state.newCity}
-            />
-          </label>
-          <input onClick={(e) => this.addCity(e)} type="submit" value="Submit" />
-        </form>
         <h1>Weather in Boston: {this.state.weather} Degrees</h1>
         <div className="clothes">Clothes</div>
       </div>
